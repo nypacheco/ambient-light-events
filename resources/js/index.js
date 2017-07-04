a7b5b0126ee8db9e1a2517a48300604281998afb
nypacheco <nathalia.yp19@gmail.com>
2017-07-03 22:58:01 -0300

    Adding new images and fix particlesJS

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,15 +1,50 @@
+var elmFace = 0;
+
+document.getElementById('container-light').style.display = 'none';
+document.getElementById('container-dark').style.display = 'none';
+
 if("ondevicelight" in window){
     window.addEventListener("devicelight", function(event) {
         if (event.value < 100) {
-            document.body.style.backgroundColor = "#141829";
-            document.getElementById('particles-light').style.display = 'none';
-            particlesJS.load('particles-dark', 'vendors/particles/config-dark.json');
+            document.getElementById('container-dark').style.display = 'flex';
+            document.getElementById('container-light').style.display = 'none';
+            $("#container-light").find("span").remove();
         } else {
-            document.body.style.backgroundColor = "#bec3ff";
-            document.getElementById('particles-dark').style.display = 'none';
-            particlesJS.load('particles-light', 'vendors/particles/config-light.json');
+            document.getElementById('container-light').style.display = 'flex';
+            document.getElementById('container-dark').style.display = 'none';
+            elmFace = $(".bat-sleeping").offset();
+            setInterval(snore, 1000);
+            snore();
         }
     });
 } else {
     alert("ondevicelight not supported");
+}
+
+function snore() {
+    var elm = document.createElement("span");
+    elm.innerText = "Z";
+    elm.setAttribute("class", "z");
+
+    posTop = elmFace.top + 230;
+    posLeft = elmFace.left + ($(".bat-sleeping").width()/2);
+    aniTop = posTop + 60;
+    aniLeft = (posLeft + 50) + Math.round(Math.random()*80);
+
+    $(elm).css({
+        "top": posTop,
+        "left": posLeft
+    });
+
+    $("#container-light").append(elm);
+    $(elm).animate({
+            "top": aniTop,
+            "left": aniLeft,
+            "font-size": "100px",
+            "opacity": 0
+        },
+        5000,
+        function() {
+            $(this).remove();
+        });
 }
